9a1c5da77105dd49e8fda3c5b17c7a078d83c140
amorospo <amorospo@yahoo.it>
2017-02-13 21:42:18 +0100

    Add files via upload

---
+++ VarOld.php
@@ -0,0 +1,14 @@
+<?php
+$site = "Sito di test new";
+$Amp = "/dev/shm/metern1.txt";
+$met_A = "1_2";
+$smtp_S = "smtp.gmail.com";
+$smtp_P = 587;
+$from_addr = "email@gmail.com";
+$pwd = "password";
+$to_addrs = "mail@mail.it";
+$Sw_Off = 0;
+$LowA = 4;
+$HiA = 14;
+$lapse = 30;
+?>
